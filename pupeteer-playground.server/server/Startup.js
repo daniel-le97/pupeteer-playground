d0740a26161ea689fa57c7ba528781b8bc6f00b9
bigmickjeans <beasticorn@gmail.com>
2021-05-08 13:37:38 -0600

    heroku server changes

--- pupeteer-playground.server/server/Startup.js
+++ pupeteer-playground.server/server/Startup.js
@@ -9,7 +9,7 @@ import { logger } from './utils/Logger'
 export default class Startup {
   static ConfigureGlobalMiddleware(app) {
     // NOTE Configure and Register Middleware
-    const whitelist = ['http://localhost:8080','http://localhost:8081']
+    const whitelist = ['http://localhost:8080','http://localhost:8081','https://weblooter.herokuapp.com/#/']
     const corsOptions = {
       origin: function(origin, callback) {
         const originIsWhitelisted = whitelist.indexOf(origin) !== -1

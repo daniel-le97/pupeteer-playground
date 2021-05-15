8e51808dafdf17c7ba0bae70b2b85f3159ede296
MickShannahan <beasticorn@gmail.com>
2021-05-14 20:11:44 -0600

    style theif for heroku

--- pupeteer-playground.client/src/pages/StyleThief.vue
+++ pupeteer-playground.client/src/pages/StyleThief.vue
@@ -39,7 +39,7 @@
 
 <script>
 import { AppState } from '../AppState'
-import { computed, reactive, onMounted } from 'vue'
+import { computed, reactive } from 'vue'
 import { queService } from '../services/QueService'
 import $ from 'jquery'
 export default {

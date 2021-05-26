8197dd8a4fb154a0aa1f59463acc4a1be29154fb
MickShannahan <beasticorn@gmail.com>
2021-05-25 18:01:38 -0600

    style theif changes

--- pupeteer-playground.server/server/controllers/StyleController.js
+++ pupeteer-playground.server/server/controllers/StyleController.js
@@ -24,41 +24,106 @@ export class StyleController extends BaseController {
         let socketRoom = req.body.socketRoom
         const browser = await puppeteer.launch(chromeOptions);
         const page = await browser.newPage();
-        page.on('console', msg => console.log('PAGE LOG:', msg.text));
+        let styleUrls =[]
+        // get all rawStyleSheets as page loads from network resources
+        await page.on('response', async response => {
+          const matches = /.*\.(css)$/.exec(response.url())
+          if(matches != null){styleUrls.push(matches[0])}
+        });
         await page.goto(url,{waitUntil: 'networkidle0', timeout:30000})
-        let allStyles = await page.evaluate(async ()=>{
-            let elementNames = ["div", "body","a", "b", "p", "h1", "h2","h3","h4","h5", "span", "ul", "li", "button", "article", "main", "header", "footer"] // Put all the tags you want colors for here
+        let allRules = {}
 
-            let elementStyles= []
-            let sheets = document.styleSheets
-            for(let i =0; i < sheets.length; i++){
-              let sheet = sheets[i]
-              for (let j = 0; j < sheet.cssRules.length; j++){
-                let rule = sheet.cssRules[j]
-                elementStyles.push(rule.cssText)
+        // compile all style sheets
+        async function markFix(){
+          return new Promise(async (resolve, reject)=>{
+            try {
+               (async function(){
+                for await(let link of styleUrls){
+                  logger.log("navigating to", link)
+             let tab = await browser.newPage().catch(err => {logger.error(err);return})
+             await tab.goto(link, {waitUntil: "networkidle0", timeout:5000}).catch(err=> logger.error(err))
+                  let rawStyle = await tab.$eval('pre', elm => elm.innerText)
+                  let flattener = /\/\*[^*]*\*+([^/*][^*]*\*+)*\/|[\n]|[\t]|[\r]/g // ----- escape /, escape *, [anything but *], any number of times, escape * at least once,([anything but /*][anything but *]anynumber of times), escape /, more than once
+                  let styleFlat = rawStyle.replace(flattener, '')
+                  let brackets = 0
+                  let lastIndex = 0
+                  for( let i= 0; i < styleFlat.length; i++){
+                  let char = styleFlat[i]
+                  switch(char){
+                    case '{':
+                    brackets++
+                    break;
+                    case '}':
+                    brackets--
+                    if(brackets == 0){
+                      let rawRule = styleFlat.slice(lastIndex, i+1)
+                      let openBracket = rawRule.indexOf('{')
+                      let selector = rawRule.slice(0, openBracket).trim()
+                      let rules = []
+                      if(selector[0]!= '@'){
+                      rules = rawRule.slice(openBracket+1, rawRule.length-1).split(';').map(e => e.trim()+ ';').filter(e => e!= ';')
+                      } else {
+                        console.log(rawRule)
+                        rules = rawRule.slice(openBracket+1, rawRule.length-1).split('}').map(e => e.trim()+'}').filter(e => e!= '}')
+                      }
+                      allRules[selector] = rules
+                      lastIndex = i+1
+                      }
+                    }
+                  }
+                  await tab.close()
               }
-            }
-
-            // elementNames.forEach( function(tagName) {
-            //   let tags = document.querySelectorAll(tagName);
-            //   tags.forEach(elm => {
-            //     let styles = elm.classList
-            //     for(let key in styles){
-            //       let style = styles[key]
-            //       elementStyles.push({[style]: document.styleSheets})
-            //     }
-            //   })
-            //   });
-              return elementStyles
-            })
+               })();
+            resolve()
+          } catch (error) {
+            reject(error)
+          }
+        })
+        }
 
         res.send({
-          message: 'Looks like we found some classes, we are compiling them, this will take about FIXME seconds', allStyles
+          message: 'Looks like we found ' + styleUrls.length + ' style sheets, we are compiling them, this will take about ' +(styleUrls.length * 3) + ' seconds',
         })
         // SECTION wait for tabs
+         await markFix()
+        await page.waitForTimeout(styleUrls.length* 3000)
+
+        // get all classes
+        let elementClasses = await page.evaluate(async ()=>{
+            const allTags = ["div", "body","a", "b", "p", "h1", "h2","h3","h4","h5", "span", "ul", "li","ol", "button", "article", "main", "header", "footer"]
+            // sorts out un-used html tags
+            let elementNames = allTags.map(t => {
+              if( document.getElementsByTagName(t).length > 0){
+                return t
+              }
+            })
+
+            // Class selectors on elements
+            let selectors = []
+            elementNames.forEach(elem => {
+              let elems = document.getElementsByTagName(elem)
+              for(let i = 0; i < elems.length; i++){
+                selectors=[...elems[i].classList, ...selectors]
+              }
+            })
+            return [...selectors.map(s => '.' + s), ...elementNames]
+            })
+            logger.log('element classes',elementClasses)
+
+        // SECTION wait for tabs
         await page.waitForTimeout( 3000)
-        logger.warn(allStyles)
-        socketService.messageRoom(socketRoom, 'style:sheet', allStyles)
+
+        // filters and formats all the css into only the used css in the DOM (currently from tag name and classes)
+        let usedClasses = {}
+        for (let key in allRules){
+          // only adds the class as a used class if the full class name(key) starts with the class selector (i)
+          if (elementClasses.find(i => key.startsWith(i))){
+            usedClasses[key] = allRules[key]
+          }
+        }
+        logger.warn('all rules',Object.keys(allRules).length,allRules )
+        logger.log('classes actual',Object.keys(usedClasses).length,usedClasses)
+        socketService.messageRoom(socketRoom, 'style:sheet', usedClasses)
         socketService.messageRoom(socketRoom, 'action:done')
         // grab stylesheets
         browser.close()
@@ -67,6 +132,8 @@ export class StyleController extends BaseController {
     }
   }
 
+
+  // SECTION get colors
   async getColors(req,res, next){
 try {
   logger.log('steal site style', req.body.url)
